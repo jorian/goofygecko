01ddafb2edd1f3488ad647a5e38cd1a43f0d6622
jorian <10341677+jorian@users.noreply.github.com>
2022-08-26 09:01:13 +0200

    wip

--- src/bot/framework.rs
+++ src/bot/framework.rs
@@ -14,14 +14,14 @@ use uuid::Uuid;
 use crate::bot::utils::database::DatabasePool;
 
 #[group("Test")]
-#[commands(ping, create_nft)]
+#[commands(delayed_ping, create_nft)]
 pub struct Test;
 
 #[command]
-// #[instrument(skip(ctx, msg))]
-async fn ping(ctx: &Context, msg: &Message) -> CommandResult {
-    info!("Message received: {}", &msg.content);
-    info!("{}", msg.author.id);
+async fn delayed_ping(ctx: &Context, msg: &Message) -> CommandResult {
+    info!("Tired, need to wait...");
+
+    tokio::time::sleep(Duration::from_secs(120)).await;
 
     msg.reply(ctx, "Pong!").await?;
 
@@ -31,9 +31,10 @@ async fn ping(ctx: &Context, msg: &Message) -> CommandResult {
 #[command]
 #[aliases(createnft)]
 async fn create_nft(ctx: &Context, msg: &Message) -> CommandResult {
-    // let span = info_span!();;
-
-    inner_create_nft(ctx, msg).await;
+    tokio::spawn(async move {
+        inner_create_nft(ctx, msg).await;
+    })
+    .await;
 
     Ok(())
 }
@@ -48,9 +49,8 @@ async fn inner_create_nft(ctx: &Context, msg: &Message) {
         "A new member joined the discord with user_id {} and discriminant {}",
         user_id, msg.author.discriminator
     );
-
     // get a sequential number to number the new gecko:
-    // let sequence = 14;
+    let sequence = 14;
     let next_gecko_number = {
         let pool = {
             let data_read = &ctx.data.read().await;
@@ -66,15 +66,20 @@ async fn inner_create_nft(ctx: &Context, msg: &Message) {
 
     debug!("creating NFT #{}", sequence);
 
-    // this process can take a while, so we spawn it in a tokio thread
-    // tokio::spawn is parallelism. It hooks into the runtime executor as a new future.
+    // info!("Tired, need to wait...");
+
+    // tokio::time::sleep(Duration::from_secs(366)).await;
+
+    let _ = msg.reply(ctx, "Pong!").await;
+
+    // // this process can take a while, so we spawn it in a tokio thread
+    // // tokio::spawn is parallelism. It hooks into the runtime executor as a new future.
     // tokio::spawn(async move {
-    // path is the location of the NFT image locally.
-    // TODO that path should be a Arweave tx
-    // if let Some(sequence) = next_gecko_number.nextval {
+    // // path is the location of the NFT image locally.
+    // // TODO that path should be a Arweave tx
+    // // if let Some(sequence) = next_gecko_number.nextval {
     match super::create_nft(user_id, sequence as u64).await {
         Ok(nft_builder) => {
-            tokio::time::sleep(Duration::from_secs(100)).await;
             msg.reply(
                 &ctx,
                 format!(
@@ -84,22 +89,25 @@ async fn inner_create_nft(ctx: &Context, msg: &Message) {
             )
             .await
             .unwrap();
+            info!("done");
 
-            if let Some(identity) = nft_builder.identity.as_ref() {
-                msg.reply(
-                    &ctx,
-                    format!(
-                        "txid of identity registration: https://testex.verus.io/tx/{}",
-                        identity.registration_txid
-                    ),
-                )
-                .await
-                .unwrap();
-            }
+            // if let Some(identity) = nft_builder.identity.as_ref() {
+            //     msg.reply(
+            //         &ctx,
+            //         format!(
+            //             "txid of identity registration: https://testex.verus.io/tx/{}",
+            //             identity.registration_txid
+            //         ),
+            //     )
+            //     .await
+            //     .unwrap();
+            // }
         }
         Err(e) => {
             error!("Something went wrong while creating the NFT: {:?}", e)
             // TODO something that notifies me
         }
     }
+    // })
+    // .await;
 }
